c292e26b52bec2db54e148123e774ae8f904ab66
Kierzene <kierzenem@gmail.com>
2026-04-18 09:51:00 +0800

    feat: scaffold complete 3-tier architecture with all controllers, views, and domain models

--- StudyGotchi/Views/MainWindow.xaml.cs
+++ StudyGotchi/Views/MainWindow.xaml.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using StudyGotchi.Controllers;
 
 namespace StudyGotchi.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private SessionController _sessionController;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void NavigateToPetSelection() { throw new System.NotImplementedException(); }
+        public void NavigateToTaskSetup() { throw new System.NotImplementedException(); }
+        public void NavigateToSettings() { throw new System.NotImplementedException(); }
+        public void NavigateToDashboard() { throw new System.NotImplementedException(); }
+        public void NavigateToSummary() { throw new System.NotImplementedException(); }
+        public void LaunchWidgetMode() { throw new System.NotImplementedException(); }
     }
 }
