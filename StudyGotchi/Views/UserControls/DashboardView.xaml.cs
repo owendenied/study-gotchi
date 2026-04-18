d6dc4392f684d79d32519636530e6b6c828f85c0
Kierzene <kierzenem@gmail.com>
2026-04-18 09:27:11 +0800

    feat: add new components and update file paths

--- StudyGotchi/Views/UserControls/DashboardView.xaml.cs
+++ StudyGotchi/Views/UserControls/DashboardView.xaml.cs
@@ -8,16 +8,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StudyGotchi.Views
+namespace StudyGotchi.Views.UserControls
 {
     /// <summary>
-    /// Interaction logic for StudyWidgetWindow.xaml
+    /// Interaction logic for DashboardView.xaml
     /// </summary>
-    public partial class StudyWidgetWindow : Window
+    public partial class DashboardView : UserControl
     {
-        public StudyWidgetWindow()
+        public DashboardView()
         {
             InitializeComponent();
         }
