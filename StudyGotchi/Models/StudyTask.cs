1a249e4c0487226b921bbaf2fddc2821bc89b483
Kierzene <kierzenem@gmail.com>
2026-04-22 14:15:03 +0800

    chore: UI fixes + widget behavior + add basic task model/controller and sprites

--- StudyGotchi/Models/StudyTask.cs
+++ StudyGotchi/Models/StudyTask.cs
@@ -7,9 +7,34 @@ namespace StudyGotchi.Models
         private string _taskName;
         private DateTime _deadline;
         private bool _isCompleted;
+        private int _id;
 
-        public void Complete() { throw new NotImplementedException(); }
-        public TimeSpan GetTimeRemaining() { throw new NotImplementedException(); }
-        public bool IsOverdue() { throw new NotImplementedException(); }
+        public StudyTask(int id, string name, DateTime deadline)
+        {
+            _id = id;
+            _taskName = name ?? string.Empty;
+            _deadline = deadline;
+            _isCompleted = false;
+        }
+
+        public int Id => _id;
+        public string Name => _taskName;
+        public DateTime Deadline => _deadline;
+        public bool IsCompleted => _isCompleted;
+
+        public void Complete()
+        {
+            _isCompleted = true;
+        }
+
+        public TimeSpan GetTimeRemaining()
+        {
+            return _deadline - DateTime.Now;
+        }
+
+        public bool IsOverdue()
+        {
+            return !_isCompleted && DateTime.Now > _deadline;
+        }
     }
 }
